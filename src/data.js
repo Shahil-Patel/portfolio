f82b2fa109b29387ada9422a09f0ddb2459ba897
Shahil Patel <shahilp@gmail.com>
2022-11-04 11:20:52 -0400

    website update
    website update

--- src/data.js
+++ src/data.js
@@ -1,48 +1,55 @@
-
 export const internList = [
-    {
-      id: 1,
-      title: "Merck",
-      subtitle: "Software Engineering Intern • Summer 2022",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-      info: "Working with Merck Animal Health’s Precision Technology Group",
+  {
+    id: 1,
+    title: "Epic Games",
+    subtitle: "Incoming Software Engineering Intern • Summer 2023",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    info: "Working with Epic Games' InfoSec Department as a Software Engineering Intern",
+  },
+  {
+    id: 2,
+    title: "Merck",
+    subtitle: "Software Engineering Intern • Summer 2022",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    info: "Utilized Python, AWS, and Dataiku on Merck’s Animal Health Precision Technology Group to manipulate agricultural and transactional data for various international clients in an Agile Work Environment. Expanded upon Python library that automates fetching, preparation, transformation, and analysis of data within Dataiku. reducing data processing time by 14%. Automated the calculation of KPIs for international agricultural facilities using lasso quantile regression, forward stepwise regression, and LQMM for use in web dashboards reducing calculation time by 22%. Designed a comprehensive web dashboard to display and simulate agricultural KPI data for clients using data generated in Dataiku and stored in AWS S3 buckets",
+  },
+  {
+    id: 3,
+    title: "Raymond James",
+    subtitle: "Technolgy Analyst Intern • Summer 2021",
+    img:
+      "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+    info: "Assisted with investment planning and asset allocation for NBA, NFL, MLB, and collegiate athletes. Used Excel VBA and Python based Web Scraping tools in order to compile specific statistics and algorithmically rank the Dow 30s best dividend securities to maximize client portfolio growth. Utilized Python and various libraries to algorithmically collect data about potential clients to facilitate faster client outreach. Automated the rebalancing of client portfolios using a combination of Python, Web Scraping tools, and Excel VBA"
     },
-    {
-      id: 2,
-      title: "Raymond James",
-      subtitle: "Data Science Intern • Summer 2021",
-      img:
-        "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
-      info: "Assisted with investment planning and asset allocation for NBA, NFL, MLB, and collegiate athletes. Used Excel VBA and Python based Web Scraping tools in order to compile specific statistics and algorithmically rank the Dow 30s best dividend securities to maximize client portfolio growth. Utilized Python and various libraries to algorithmically collect data about potential clients to facilitate faster client outreach. Automated the rebalancing of client portfolios using a combination of Python, Web Scraping tools, and Excel VBA"
-      },
-  ];
-  
-  export const researchList = [
-    {
-      id: 1,
-      title: "Rutgers University - AI in Transportation",
-      subtitle: "Research Assistant • September 2022 - April 2023",
-      img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
-      info: "Working with Professor Xiang Liu on using Artificial Intelligence for Next-Generation Intelligent Transportation Systems"
+];
+
+export const researchList = [
+  {
+    id: 1,
+    title: "Rutgers University - AI in Transportation",
+    subtitle: "Research Assistant • September 2022 - April 2023",
+    img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    info: "Working with Professor Xiang Liu on using Artificial Intelligence for Next-Generation Intelligent Transportation Systems"
+  },
+];
+
+export const clubList = [
+  {
+    id: 1,
+    title: "Quantitative Finance Club (QFC)",
+    subtitle: "Member • 2020 - Current",
+    img:
+      "https://cdn.dribbble.com/users/2597268/screenshots/15468893/media/44313553d73ba41580f1df69749bba28.jpg?compress=1&resize=1200x900",
+    info: "Utilized QuantConnect to create Algorithmic Trading Algorithms coded in Python. Created an Algorithm based on SMA and Golden Cross Theory and backtested it against 10 years of trading data. Made it to semi-finals of the QFC Club's algo trading competition"
     },
-  ];
-  
-  export const clubList = [
-    {
-      id: 1,
-      title: "Quantitative Finance Club (QFC)",
-      subtitle: "Member • 2020 - Current",
-      img:
-        "https://cdn.dribbble.com/users/2597268/screenshots/15468893/media/44313553d73ba41580f1df69749bba28.jpg?compress=1&resize=1200x900",
-      info: "Utilized QuantConnect to create Algorithmic Trading Algorithms coded in Python. Created an Algorithm based on SMA and Golden Cross Theory and backtested it against 10 years of trading data. Made it to semi-finals of the QFC Club's algo trading competition"
-      },
-    {
-      id: 2,
-      title: "Undergraduate Student Alliance of Computer Scientists (USACS)",
-      subtitle: "Member • 2020 - Current",
-      img:
-        "https://cdn.dribbble.com/users/5920881/screenshots/15463142/media/b5a460223798bd85d835710806e2f3dd.png?compress=1&resize=1200x900",
-      info: "Attended various hackathons including HackRU 2022 where my group won Best Google Cloud Project (Athena)"
-    },
-  ];
+  {
+    id: 2,
+    title: "Undergraduate Student Alliance of Computer Scientists (USACS)",
+    subtitle: "Member • 2020 - Current",
+    img:
+      "https://cdn.dribbble.com/users/5920881/screenshots/15463142/media/b5a460223798bd85d835710806e2f3dd.png?compress=1&resize=1200x900",
+    info: "Attended various hackathons including HackRU 2022 where my group won Best Google Cloud Project (Athena)"
+  },
+];
