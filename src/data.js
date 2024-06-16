a68595db5d92643f628bc53620c997543e9b5527
Shahil Patel <shahilp@gmail.com>
2024-06-16 19:02:58 -0400

    More updates

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const workList = [
   {
     id: 1,
     title: "Epic Games",
-    subtitle: "Software Engineer • May 2023 - Current",
+    subtitle: "Software Engineer (Application Engineering Team) • May 2023 - Current",
     img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
     info: "Built an internal Full-Stack Okta Application that provides multiple teams with an easy way to query Snowflake data for use in legal matter creations, streamlining the process by 47% while using Golang, AWS, Terraform, React, and Docker/Kubernetes. Developed a Snowflake database abstraction layer module as part of an internal Golang library that provides 9+ teams and over 100+ employees with a way to seamlessly integrate Snowflake into internal tools, automation, and projects with minimal code. Created an internal service that automatically updates Splunk lookup tables with current AWS Stewards data for 3+ teams and over 16+ employees using Go, Python, GitHub Actions, Terraform, and AWS while on the Application Engineering team"
   },
@@ -12,7 +12,7 @@ export const internList = [
     {
       id: 1,
       title: "Merck & Co",
-      subtitle: "Software Engineering Intern • Summer 2022",
+      subtitle: "Software Engineering Intern (Precision Technology Dev Team) • Summer 2022",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
       info: "Automated the calculation of KPIs in Dataiku for international agricultural facilities using lasso quantile regression, forward stepwise regression, and LQMM for use in web dashboards, reducing calculation and analysis time by 22%. Designed a comprehensive React web dashboard with multiple visualizations to display and simulate agricultural KPI data for clients using data generated in Dataiku and stored in AWS S3 buckets for 200+ clients across the world. Collaborated with 3+ teams while on Merck’s Animal Health Precision Technology Group to work on a multi-year project which utilized agricultural and transactional data for various international clients in an Agile work environment",
@@ -20,7 +20,7 @@ export const internList = [
     {
       id: 2,
       title: "Raymond James Financial",
-      subtitle: "Technology Analyst Intern • Summer 2021",
+      subtitle: "Technology Analyst Intern (Seiler Group) • Summer 2021",
       img:
         "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
       info: "Developed an automation to algorithmically rank the Dow 30's best dividend securities using Excel VBA, Python, and the IEX API. Automated the rebalancing of client portfolios using Python and Excel VBA increasing rebalancing speed by 90%. Algorithmically collected data about potential clients to facilitate 52% faster client outreach using Python and various libraries"
@@ -31,7 +31,7 @@ export const internList = [
     {
       id: 1,
       title: "Rutgers University - AI in Transportation",
-      subtitle: "Research Assistant • September 2022 - April 2023",
+      subtitle: "Research Assistant (with Professor Xiang Liu) • September 2022 - December 2023",
       img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
       info: "Retrained multiple custom YOLOv5 models using PyTorch, hyper-parameter tuning, dataset splitting, scaling, and classifier training reducing the models’ false positive rate on average by 15% and false negative rate by 19% in 3 different object classes. Expanded upon a Python webapp that generates geolocational and trespassing visualizations, optimizing calculation/display delay by 21% and improve rendering efficiency by 16% while using Flask, Dash, and trespassing data for 5+ researchers. Built CI/CD pipelines for edge servers on a fleet of 20+ trains as well as for various frontend websites and dashboards, streamlining the process for updating multiple services using AWS CodeBuild, CodeDeploy, and CodePipeline"
     },
@@ -54,12 +54,4 @@ export const internList = [
         "https://cdn.dribbble.com/users/5920881/screenshots/15463142/media/b5a460223798bd85d835710806e2f3dd.png?compress=1&resize=1200x900",
       info: "Attended various hackathons including HackRU 2022 where my group won Best Google Cloud Project (Athena) as well as won Best Rutgers Hack at HackRU 2023 (RuPlatePal) "
     },
-  {
-    id: 2,
-    title: "Undergraduate Student Alliance of Computer Scientists (USACS)",
-    subtitle: "Member • 2020 - Current",
-    img:
-      "https://cdn.dribbble.com/users/5920881/screenshots/15463142/media/b5a460223798bd85d835710806e2f3dd.png?compress=1&resize=1200x900",
-    info: "Attended various hackathons including HackRU 2022 where my group won Best Google Cloud Project (Athena)"
-  },
 ];
